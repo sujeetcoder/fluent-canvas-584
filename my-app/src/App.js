6e6b46173e08c789e3c3ebf92445453b4c76a318
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-08-23 17:50:54 +0530

    first day

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import top from "./img/top_nav.png"
+import css from "./componant/new.module.css"
+import Navbar from "./componant/navbar"
+import Home from "./componant/home"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import News from './componant/news';
 
 function App() {
   return (
     <div className="App">
+      <img className={css.image1}  src={top} />
+      <Navbar/>
       
+         <Routes>
+            <Route path={'/'} element={<Home/>} />
+            <Route path={'/news'} element={<News/>} />
+         </Routes>
+      
+
+
+
     </div>
   );
 }
---
+++ my-app/src/componant/news.js
@@ -0,0 +1,9 @@
+import css from "./new.module.css"
+
+export default function News(){
+    return (
+        <div className={css.News} >
+            <h1>hello</h1>
+        </div>
+    )
+}
