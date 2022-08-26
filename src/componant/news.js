49d7736e7a0f47fb67795895139b76a59500a9f9
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-08-26 19:34:15 +0530

    done

--- src/componant/news.js
+++ src/componant/news.js
@@ -1,10 +1,22 @@
 import '../App.css';
 import "./news.css"
 import news_top from "../img/news_top.png"
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function News(){
+    let navigate = useNavigate()
+    const [loading,setLoading] = useState(true)
+
+    setTimeout(() => {
+        setLoading(false)
+    }, 1200);
 
     let obj = JSON.parse(localStorage.getItem("obj")) || {}
+
+    if(loading){
+        return <h1 className='loading_news' >Loading..</h1>
+    }
 
     if(obj.title!=undefined){
         return (
@@ -13,11 +25,12 @@ export default function News(){
             <img className='news_top' src={news_top}/>
             <h1 className='title' >{obj.title}</h1>
             <p className='title' > {obj.description} </p>
-            <p className='pub' > {obj.publishedAt} </p>
+            <p className='pub' > <span className='news_name' onClick={()=>{ window.location.href = obj.url }} > {obj.source.name} </span> {obj.publishedAt} </p>
             <div className='news' >
                 <div className='news_first' >
                     <img className='news_img' src={obj.urlToImage} />
                     <p className='img_desc' > {obj.description} </p>
+                    <p> {obj.description} {obj.content} </p>
                 </div>
 
                 <div className='news_sec' ></div>
@@ -25,7 +38,8 @@ export default function News(){
             </div>
             </>
         )
-    }
+    } 
+
 
     return (
         <div className="News" >
